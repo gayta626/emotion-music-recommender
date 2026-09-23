7e2d8032f1dd28792258221b78a9173225798810
gayta626 <nguyenducvinh0601@gmail.com>
2026-09-23 11:34:31 +0700

    extra

---
+++ emotune-frontend/src/components/EmotionScanner.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
+
+const EmotionScanner = () => {
+    const videoRef = useRef(null);
+    const canvasRef = useRef(null);
+    const [isActive, setIsActive] = useState(false);
+
+    useEffect(() => {
+        let stream;
+
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then((mediaStream) => {
+                stream = mediaStream;
+                videoRef.current.srcObject = mediaStream;
+                setIsActive(true);
+            })
+            .catch((err) => {
+                console.error("Loi :" + err)
+            })
+
+        const captureAndSend = () => {
+            if (!videoRef.current || !canvasRef.current) return;
+
+            const canvas = canvasRef.current;
+            const video = videoRef.current;
+            canvas.width = 224;
+            canvas.height = 224;
+            const ctx = canvas.getContext("2d");
+            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+            const base64Image = canvas.toDataURL("image/jpeg", 0.8);
+
+            axios.post('http://localhost:8080/scan-and-suggest', {
+                image: base64Image
+            })
+                .then((response) => {
+                    console.log("Ket qua:", response.data);
+
+                })
+                .catch((err) => {
+                    console.error("Loi goi API:", err);
+                });
+        }
+
+        const intervalId = setInterval(captureAndSend, 3000);
+
+        return () => {
+            clearInterval(intervalId);
+            if (stream) {
+                stream.getTracks().forEach(track => track.stop());
+            }
+            setIsActive(false);
+        }
+    }, [])
+
+    return (
+        <>
+            {isActive && <span>🔴</span>}
+            <video ref={videoRef} autoPlay style={{ display: "none" }} />
+            <canvas ref={canvasRef} style={{ display: "none" }} />
+        </>
+    )
+}
+
+export default EmotionScanner
